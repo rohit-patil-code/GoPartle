5b410d678d562b30de33edd6153cb981a417d523
Rohit <rohitpatilwork7797@gmail.com>
2026-01-31 11:29:56 +0530

    Add domain to cors policy in backend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://go.rohitcodes.tech'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Create Requirement Route
